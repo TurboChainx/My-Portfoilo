8a248185f0d4c048275a39e3e706fafe062e2a50
blueunicorn0805 <blueunicorn0805@gmail.com>
2024-11-17 17:46:57 +0700

    fix : overview width

--- src/components/sections/About.tsx
+++ src/components/sections/About.tsx
@@ -50,7 +50,7 @@ const About = () => {
 
       <motion.p
         variants={fadeIn("", "", 0.1, 1)}
-        className="text-secondary mt-4 max-w-3xl text-[17px] leading-[30px] w-[80%] mx-auto text-center"
+        className="text-secondary mt-4 text-[17px] leading-[30px] w-[80%]"
       >
         {config.sections.about.content}
       </motion.p>
